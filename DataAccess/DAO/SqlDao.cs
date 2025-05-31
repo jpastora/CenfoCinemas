ce8844447e225a4181ec4b906ef550d7f5e8487b
Joseph Pastora <joseph.pastora@gmail.com>
2025-05-31 11:37:58 -0600

    Refactor data access and DTO structure
    - Removed `Class1` from `DTOs` and `DataAccess` namespaces.
    - Added `CRUD` folder to the project structure.
    - Introduced `SqlDao` class implementing Singleton pattern for data access.
    - Added `SqlOperation` class for managing stored procedure details.
    - Created `BaseDTO` class with common properties for DTOs.
    - Added `Movie` class inheriting from `BaseDTO` with relevant properties.
    - Introduced `User` class inheriting from `BaseDTO` with user-related properties.

---
+++ DataAccess/DAO/SqlDao.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+
+    /*
+     * Unico objetos de  acceso a datos que se pueden usar en el proyecto
+     * Solo ejecuta Store Procedures 
+     * 
+     * Esta clase implementa el patron del Singleton
+     * para asegurar que solo haya una instancia de este objeto
+     * 
+     */
+
+    public class SqlDao
+    {
+
+        // Paso 1: Crear una instancia privada de la misma clase
+        private static SqlDao _instance;
+
+        private string _connectionString;
+
+        // Paso 2: Redefinir el constructor default para que sea privado
+
+        private SqlDao()
+        {
+            // Aquí puedes inicializar la cadena de conexión
+            _connectionString = string.Empty; // Reemplaza con tu cadena de conexión real
+        }
+
+        // Paso 3: Crear un método público estático para obtener la instancia
+
+        public static SqlDao GetInstance()
+        {
+            // Si la instancia no existe, crearla
+            if (_instance == null)
+            {
+                _instance = new SqlDao();
+            }
+            // Devolver la instancia
+            return _instance;
+
+        }
+
+        // Paso 4: Crear un método para ejecutar Store Procedures
+        public void ExecuteStoredProcedure(SqlOperation operation)
+        {
+  
+
+        }
+
+        //Metodo para la ejecuacion Store Procedures con retorno de datos
+        public List<Dictionary<string, object>> ExecuteQueryProcedure(SqlOperation operation)
+        {
+            //Conexion a la base de datos y ejecucion del Store Procedure
+            //Capturar el resultado en una lista de diccionario
+            //Convertirlo en DTOs
+            var list = new List<Dictionary<string, object>>();
+
+            return list;
+
+        }
+}
